423b74857c6b73b9b819562d102b30f452c0c7e9
guojunhao118 <guojunhao118@gmail.com>
2020-06-28 20:36:51 +0800

    初始化

---
+++ src/app.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="main">
+        name : {{ text }}
+        <img :src="avatorImg" alt="" class="img">
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: 'vue start',
+            avatorImg: require('./assets/image/avator.jpeg')
+        }
+    }
+}
+</script>
+
+<style lang='scss'>
+    .main {
+        background: green;
+        .img {
+            width: 100px;
+            height: 100px;
+        }
+    }
+</style>
